d96efed57c8c6c57792039ea4a7b607a2faefba3
Tony Tam <fehguy@gmail.com>
2012-11-29 11:49:20 -0800

    updated to swagger-core-1.2.0

--- src/main/java/com/axemblr/dropwizard/swagger/SwaggerBundle.java
+++ src/main/java/com/axemblr/dropwizard/swagger/SwaggerBundle.java
@@ -3,6 +3,8 @@ package com.axemblr.dropwizard.swagger;
 import com.yammer.dropwizard.bundles.AssetsBundle;
 import com.yammer.dropwizard.config.Environment;
 
+import com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON;
+
 public class SwaggerBundle extends AssetsBundle {
 
     public static final String DEFAULT_PATH = "/swagger-ui";
@@ -14,6 +16,6 @@ public class SwaggerBundle extends AssetsBundle {
     @Override
     public void initialize(Environment environment) {
         super.initialize(environment);
-        environment.addResource(new ApiListingResource());
+        environment.addResource(new ApiListingResourceJSON());
     }
 }
